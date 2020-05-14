241398db63f51c01ad8de2b66dd14b7b5dd44ea4
Unknown <0994624@hr.nl>
2020-05-15 00:12:29 +0200

    AddCustomer Class Made

--- PLS.py
+++ PLS.py
@@ -4,11 +4,48 @@ def StartMenu():
     if MainMenuChoice == "1":
         print("Mooi")
     elif MainMenuChoice == "5":
-        AddCustomer
+        NewCustomer()
+
+def NewCustomer():
+    print("\nPlease enter the user's number.")
+    aNumber = input()
+    print("\nPlease enter the user's gender.")
+    aGender = input()
+    print("\nPlease enter the user's nameset.")
+    aNameSet = input()
+    print("\nPlease enter the user's given name.")
+    aGivenName = input()
+    print("\nPlease enter the user's surname.")
+    aSurname = input()
+    print("\nPlease enter the user's street adress")
+    aStreetAddress = input()
+    print("\nPlease enter the user's zipcode")
+    aZipCode = input()
+    print("\nPlease enter the user's city")
+    aCity = input()
+    print("\nPlease enter the user's email address")
+    aEmailAddress = input()
+    print("\nPlease enter the user's username")
+    aUsername = input()
+    print("\nPlease enter the user's telephonenumber")
+    aTelephoneNumber = input()
+    newUser = AddCustomer(aNumber, aGender, aNameSet, aGivenName, aSurname, aStreetAddress, aZipCode, aCity, aEmailAddress, aUsername, aTelephoneNumber)
+
 
 
 class AddCustomer():
-    def __init__
+    def __init__(self, Number, Gender, NameSet, GivenName, Surname, StreetAddress, ZipCode, City, EmailAddress, Username, TelephoneNumber):
+        self.Number = Number
+        self.Gender = Gender
+        self.NameSet = NameSet
+        self.GivenName = GivenName
+        self.Surname = Surname
+        self.StreetAddress = StreetAddress
+        self.ZipCode = ZipCode
+        self.City = City
+        self.EmailAddress = EmailAddress
+        self.Username = Username
+        self.TelephoneNumber = TelephoneNumber
 
 
 print("Welcome to the library system")
