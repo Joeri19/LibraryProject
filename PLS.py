06ea2f411d12ac67e4be4a79a03c4848b1353ab3
Unknown <0994624@hr.nl>
2020-05-14 21:08:18 +0200

    Hoofdmenu

--- PLS.py
+++ PLS.py
@@ -1,3 +1,15 @@
-def Start():
-    print("Welcome to the library system.\nWhat would you like to do?\n1 - ")
-    
+def StartMenu():
+    print("What would you like to do?\n\n1 - Search for a book\n2 - Loan a book\n3 - Return a loaned book\n4 - Add a book\n5 - Add a customer\n6 - Make a backup of the system\n7 - Restore the system from a backup\n8 - Quit the program")
+    MainMenuChoice = input()
+    if MainMenuChoice == "1":
+        print("Mooi")
+    elif MainMenuChoice == "5":
+        AddCustomer
+
+
+class AddCustomer():
+    def __init__
+
+
+print("Welcome to the library system")
+StartMenu()
